70d46271be7437019877ca360bbcde7bcc9c75ae
ranzhizhong <ranzz_2002@163.com>
2026-07-27 18:03:44 +0800

    PERF: avoid allocation in groupby reduceat guard

--- pandas/core/groupby/ops.py
+++ pandas/core/groupby/ops.py
@@ -422,7 +422,17 @@ class WrappedCythonOp:
             if (
                 comp_ids[0] == 0
                 and comp_ids[-1] == ngroups - 1
-                and (len(comp_ids) < 8 or (np.diff(comp_ids[:8]) >= 0).all())
+                and (
+                    len(comp_ids) < 8
+                    or comp_ids[0]
+                    <= comp_ids[1]
+                    <= comp_ids[2]
+                    <= comp_ids[3]
+                    <= comp_ids[4]
+                    <= comp_ids[5]
+                    <= comp_ids[6]
+                    <= comp_ids[7]
+                )
             ):
                 diff = np.diff(comp_ids)
             else:
@@ -465,7 +475,17 @@ class WrappedCythonOp:
             if (
                 comp_ids[0] == 0
                 and comp_ids[-1] == ngroups - 1
-                and (len(comp_ids) < 8 or (np.diff(comp_ids[:8]) >= 0).all())
+                and (
+                    len(comp_ids) < 8
+                    or comp_ids[0]
+                    <= comp_ids[1]
+                    <= comp_ids[2]
+                    <= comp_ids[3]
+                    <= comp_ids[4]
+                    <= comp_ids[5]
+                    <= comp_ids[6]
+                    <= comp_ids[7]
+                )
             ):
                 diff = np.diff(comp_ids)
             else:
