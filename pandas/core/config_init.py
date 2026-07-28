76aebfeb8a3f00ab20c11c6de5e1a14e939702c8
uEchoZ <812303785@qq.com>
2026-07-28 14:25:56 +0800

    PERF: enable SwissTable on aarch64 by default

--- pandas/core/config_init.py
+++ pandas/core/config_init.py
@@ -29,6 +29,7 @@ from pandas._config.config import (
 )
 
 from pandas.errors import Pandas4Warning
+from pandas.compat._arch import IS_ARM
 
 # compute
 
@@ -77,12 +78,11 @@ def use_numba_cb(key: str) -> None:
 use_swisstable_doc = """
 : bool
     Use Swiss Tables (SIMD-accelerated hash tables) for supported hash table
-    operations. This is experimental. Default is False.
+    operations. This is experimental. Default is True on aarch64, other is False.
     Valid values: False,True
 """
 
-
-_use_swisstable: bool = False
+_use_swisstable: bool = True if IS_ARM else False
 
 
 def use_swisstable_cb(key: str) -> None:
@@ -110,7 +110,7 @@ with cf.config_prefix("compute"):
     )
     cf.register_option(
         "use_swisstable",
-        False,
+        True if IS_ARM else False,
         use_swisstable_doc,
         validator=is_bool,
         cb=use_swisstable_cb,
