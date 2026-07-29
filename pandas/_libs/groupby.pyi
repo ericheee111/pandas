aeea422e8cf84de8d8ebef3e009af9c08433450b
luozisheng_ <2486564466@qq.com>
2026-07-29 11:51:50 +0800

    PERF: fuse nth dropna mask on AArch64

--- pandas/_libs/groupby.pyi
+++ pandas/_libs/groupby.pyi
@@ -7,6 +7,7 @@ from pandas._typing import npt
 def group_nth_zero_mask(
     labels: npt.NDArray[np.intp],
     ngroups: int,
+    valid: npt.NDArray[np.bool_] | None = ...,
 ) -> npt.NDArray[np.bool_]: ...
 def group_median_float64(
     out: np.ndarray,  # ndarray[float64_t, ndim=2]
--- pandas/_libs/groupby.pyx
+++ pandas/_libs/groupby.pyx
@@ -79,17 +79,22 @@ cdef enum InterpolationEnumType:
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def group_nth_zero_mask(const intp_t[::1] labels, Py_ssize_t ngroups):
-    """Return a mask selecting the first non-null row from each group."""
+def group_nth_zero_mask(
+    const intp_t[::1] labels,
+    Py_ssize_t ngroups,
+    const uint8_t[::1] valid=None,
+):
+    """Return a mask selecting the first valid row from each group."""
     cdef:
         Py_ssize_t i, lab, n = len(labels)
+        bint check_valid = valid is not None
         uint8_t[::1] seen = np.zeros(ngroups, dtype=np.uint8)
         uint8_t[::1] result = np.zeros(n, dtype=np.uint8)
 
     with nogil:
         for i in range(n):
             lab = labels[i]
-            if lab >= 0 and seen[lab] == 0:
+            if (not check_valid or valid[i]) and lab >= 0 and seen[lab] == 0:
                 seen[lab] = 1
                 result[i] = 1
 
