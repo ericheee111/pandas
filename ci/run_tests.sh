3fe6ace5bd224ff6ffb48cccbac304aa7839dfba
kwf <wkf213696@163.com>
2026-08-06 14:46:40 +0800

    TST: pin CI workers to 8 and exclude known-failing tests

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -5,7 +5,7 @@ set -e
 SOURCE_DIR=$(cd "$(dirname "$0")/.." && pwd)
 
 if [[ -z "${PYTEST_WORKERS+x}" ]]; then
-  PYTEST_WORKERS=auto
+  PYTEST_WORKERS=8
 fi
 if [[ -z "${PYTEST_TARGET+x}" ]]; then
   PYTEST_TARGET=pandas
